9288ab179c402cbc438915d1b9a4bac3cdb1605e
Miranda <lfelipee.mi@gmail.com>
2025-02-26 14:02:56 -0300

    animação do passaro

--- v1/js/flappy.js
+++ v1/js/flappy.js
@@ -90,11 +90,59 @@ function criarBarreiras(altura, largura, abertura, espaco, notificarPonto) {
   };
 }
 
+function Passaro(alturaJogo) {
+  let voando = false;
+  /* let podeVoar = true; */
+
+  this.elemento = novoElemento("img", "passaro");
+  this.elemento.src = "midia/passaro.png";
+
+  this.getY = () => parseInt(this.elemento.style.bottom.split("px")[0]);
+  this.setY = (y) => (this.elemento.style.bottom = `${y}px`);
+
+  window.onkeydown = (e) => (voando = true);
+  window.onkeyup = (e) => (voando = false);
+
+  /*
+  window.onkeydown = (e) => {
+    if ((e.code === "Space" || e.key === "ArrowUp") && podeVoar) {
+      voando = true;
+      podeVoar = false;
+    }
+  };
+
+  window.onkeyup = (e) => {
+    if (e.code === "Space" || e.key === "ArrowUp") {
+      voando = false;
+      podeVoar = true;
+    }
+  };*/
+
+  this.animarP = () => {
+    const novoY = this.getY() + (voando ? 9 : -8);
+    const alturaMaxima = alturaJogo - this.elemento.clientHeight;
+
+    if (novoY <= 0) {
+      this.setY(0);
+    } else if (novoY >= alturaMaxima) {
+      this.setY(alturaMaxima);
+    } else {
+      this.setY(novoY);
+    }
+  };
+
+  this.setY(alturaJogo / 2);
+}
+
 const alturaEmPixels = (86 / 100) * window.innerHeight;
 const larguraEmPixels = (96 / 100) * window.innerWidth;
 const barreiras = new criarBarreiras(alturaEmPixels, larguraEmPixels, 200, 400);
+const passaro = new Passaro(alturaEmPixels);
 const areaDoJogo = document.querySelector("[flappy]");
+
+areaDoJogo.appendChild(passaro.elemento);
 barreiras.pares.forEach((par) => areaDoJogo.appendChild(par.elemento));
 setInterval(() => {
   barreiras.animar();
+  passaro.animarP();
 }, 20);
